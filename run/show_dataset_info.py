62f4b691e22828d648201d4359f1aa01ec91c950
kyle <kyle@wrtn.io>
2025-05-28 18:51:58 +0900

    fix: no temporary memory saving

--- run/show_dataset_info.py
+++ run/show_dataset_info.py
@@ -5,12 +5,27 @@ data_dir = "../data"
 
 def show_info(file_name):
     # show corpus/vocab/
+    word_cnt = 0
+    word_set = set()
+    word_buf = []
     # dataset size, vocab size, corpus size
     with open(f"{data_dir}/{file_name}", "r", encoding="utf-8") as f:
-        words = f.read().split()
+
+        while True:
+            char = f.read(1)
+            if not char:
+                break
+            if char.isspace():
+                word = "".join(word_buf)
+                word_set.add(word)
+                word_buf.clear()
+                word_cnt += 1
+            else:
+                word_buf.append(char)
+
     print(f"Dataset file: {data_dir}/{file_name}")
-    print(f"Total words in dataset: {len(words)}")
-    print(f"Unique words in dataset: {len(set(words))}")
+    print(f"Total words in dataset: {word_cnt}")
+    print(f"Unique words in dataset: {len(word_set)}")
 
 
 if __name__ == "__main__":
