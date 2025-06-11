7c5a6aedf75a7b07052e5622e6f3a62a4d8d2f9a
kyle <kyle@wrtn.io>
2025-06-11 17:06:31 +0900

    fix: argument error

--- scripts/split-dataset.sh
+++ scripts/split-dataset.sh
@@ -3,6 +3,7 @@
 # ./split_units.sh ../data/data.txt 240K 480M 6G
 
 INPUT=$1
+shift
 WORDS=("$@")
 
-python ../run/split_dataset.py --input "$INPUT" "$WORDS[@]"
+python ../run/split_dataset.py --input "$INPUT" "$WORDS"
