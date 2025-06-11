bf453cce47d40d43ff0aa5a927fae439869e942b
kyle <kyle@wrtn.io>
2025-06-11 18:07:51 +0900

    fix

--- run/split_dataset.py
+++ run/split_dataset.py
@@ -49,7 +49,7 @@ def main():
     args = parser.parse_args()
 
     input_path = args.input
-    unit_args = args.units.split(" ")[0]
+    unit_args = args.units[0].split(" ")
     units = [parse_unit(u) for u in unit_args]
     units_flag = [0] * len(units)
 
