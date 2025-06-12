15d58abed2267ec80a9b63d11589c8f2d951a6f9
kyle <kyle@wrtn.io>
2025-06-12 15:09:53 +0900

    fix

--- run/split_dataset.py
+++ run/split_dataset.py
@@ -53,14 +53,17 @@ def main():
     unit_args = args.units[0].split(" ")
     units = [{"value": parse_unit(u), "unit_args": u} for u in unit_args]
 
+    removed_units = []
     for i, unit in enumerate(units):
         output_path = f"../data/14b_{unit_args[i]}.txt"
         if os.path.exists(output_path):
-            units.remove(unit)
+            removed_units.append(unit)
             print(f"[run/split_dataset.py] {output_path} already exists, skipping.")
         else:
             print(f"[run/split_dataset.py] {output_path} does not exist, will create.")
 
+    for unit in removed_units:
+        units.remove(unit)
     print(units)
     with open(input_path, "r", encoding="utf-8") as f:
         word_gen = read_words_from_stream(f)
@@ -81,14 +84,14 @@ def main():
             # Skip empty words
             if not words.strip():
                 continue
-
+            removed_units = []
             for i, word_unit_info in enumerate(units):
                 unit_str = word_unit_info["unit_args"]
                 output_path = f"../data/14b_{unit_str}.txt"
 
                 if count > word_unit_info["value"]:
                     # units.remove(word_limit)
-                    units.remove(word_unit_info)
+                    removed_units.append(word_unit_info)
 
                     print(
                         f"[run/split_dataset.py] Reached limit for {unit_str}, skipping."
@@ -102,6 +105,8 @@ def main():
                         out.write(words)
                     except StopIteration:
                         break
+            for unit in removed_units:
+                units.remove(unit)
 
 
 if __name__ == "__main__":
