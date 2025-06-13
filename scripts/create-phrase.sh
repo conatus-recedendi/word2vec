eb92c3bd6adf8374d0561321f7beb78845ef10be
kyle <kyle@wrtn.io>
2025-06-13 17:55:58 +0900

    fix

--- scripts/create-phrase.sh
+++ scripts/create-phrase.sh
@@ -4,7 +4,7 @@
 #  p2_table3.sh: threhshold 200, 100, 50, size=1B
 #  p2_exp1.sh: threhshold 200, 100, 50, size=6B
 THRESHOLD=( 200 100 50 )
-SIZE = 1B # 
+SIZE=1B # 
 DATASET="../data/14b_$SIZE.txt"
 STEP=2 # bi-gram. 3 means tri-gram ...
 
