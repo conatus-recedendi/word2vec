0d8f459d30467b7408afc262c01f9b1a0570ce8e
kyle <kyle@wrtn.io>
2025-06-25 10:26:41 +0900

    Fix

--- experiement/p1_exp2_show_vocab.sh
+++ experiement/p1_exp2_show_vocab.sh
@@ -2,5 +2,18 @@
 
 # bash ./p1_exp2_show_vocab.sh ../data/14b_783M.txt ../data/14b_24M.txt --file_base "../data/questions-words.txt"
 
+log_time() {
+        logfile="$1"
+        shift
+        echo "Running: $*" | tee -a "$logfile"
+        start=$(date +%s)
+        "$@" 2>&1 | tee /dev/tty | awk 'index($0, "\r") == 0' >> "$logfile"
+        end=$(date +%s)
+        echo "Time elapsed: $((end - start))s" | tee -a "$logfile"
+        echo "" | tee -a "$logfile"
+}
 
-python ../run/p1_exp2_show_vocab.py --file1 "$1" --file2 "$2" --file_base "$3" --topk 30000
+LOG_FILE="../output/p1_exp2_show_vocab_$(date +"%Y%m%d_%H%M").log"
+
+
+log_time $LOG_FILE python ../run/p1_exp2_show_vocab.py --file1 "$1" --file2 "$2" --file_base "$3" --topk 30000
