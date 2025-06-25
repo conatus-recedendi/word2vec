7ad1599f7afd7eff17fed4e4e9162c6e339a5fc6
kyle <kyle@wrtn.io>
2025-06-25 17:48:14 +0900

    fix

--- scripts/show-database-info.sh
+++ scripts/show-database-info.sh
@@ -2,7 +2,19 @@
 
 # ./show-database-info.sh data.txt data2.txt
 
+log_time() {
+        logfile="$1"
+        shift
+        echo "Running: $*" | tee -a "$logfile"
+        start=$(date +%s)
+        "$@" 2>&1 | tee /dev/tty | awk 'index($0, "\r") == 0' >> "$logfile"
+        end=$(date +%s)
+        echo "Time elapsed: $((end - start))s" | tee -a "$logfile"
+        echo "" | tee -a "$logfile"
+}
+
+
+LOG_FILE="../output/${i%}.info"
 for i in "$@"; do
-  touch "../output/${i%}.info"
-  python ../run/show_dataset_info.py --file ${i%}  | tee /dev/tty | awk 'index($0, "\r") == 0' >> "../output/${i%}.info"
+  log_time $LOG_FILE python ../run/show_dataset_info.py --file ${i%}  | tee /dev/tty | awk 'index($0, "\r") == 0'  
 done
