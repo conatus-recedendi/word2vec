640b51ec8bc7a0f6d2bf42f26ad0524c866c22ec
kyle <kyle@wrtn.io>
2025-06-25 10:23:55 +0900

    fix

--- experiement/p1_exp2_show_vocab.sh
+++ experiement/p1_exp2_show_vocab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# bash ./p1_exp2_show_vocab.sh ../data/14b_783M.txt ../data/14b_24M.txt
+# bash ./p1_exp2_show_vocab.sh ../data/14b_783M.txt ../data/14b_24M.txt --file_base "../data/questions-words.txt"
 
 
-python ../run/p1_exp2_show_vocab.py --file1 "$1" --file2 "$2" --topk 30000
+python ../run/p1_exp2_show_vocab.py --file1 "$1" --file2 "$2" --file_base "$3" --topk 30000
