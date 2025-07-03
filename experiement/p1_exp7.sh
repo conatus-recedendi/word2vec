4a823fe3ca4d614f19c3e16b64f1d8daeb2b3224
kyle <kyle@wrtn.io>
2025-07-03 11:17:50 +0900

    fix

--- experiement/p1_exp7.sh
+++ experiement/p1_exp7.sh
@@ -29,10 +29,10 @@ bash ../scripts/split-dataset.sh "$DATASET" 783M 1.6B
 # 조합 리스트 (형식: "iter dim size model")
 combinations=(
   # "3 300 783M cbow"
-  "1 300 783M skip-gram 2"
-  "1 300 783M skip-gram 3"
-  "1 300 783M skip-gram 5"
-  "1 300 783M skip-gram 6"
+  "1 300 783M cbow 2"
+  "1 300 783M cbow 3"
+  "1 300 783M cbow 5"
+  "1 300 783M cbow 6"
   # "1 300 783M cbow"
   # "1 300 1.6B cbow"
   # "1 600 783M cbow"
